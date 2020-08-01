85be6ae4660dcf057ed79c4958d872b64b83ee9e
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-08-02 02:09:34 +0530

    Added Helper

---
+++ lib/helper/data.dart
@@ -0,0 +1,56 @@
+import '../models/category.dart';
+
+List<Category> getCategories() {
+  List<Category> category = new List<Category>();
+  Category categoryObject = new Category();
+  //1
+  categoryObject.categoryName = "Business";
+  categoryObject.imageUrl =
+      "https://images.unsplash.com/photo-1526304640581-d334cdbbf45e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60";
+
+  category.add(categoryObject);
+
+  //2
+  categoryObject = Category();
+  categoryObject.categoryName = "Entertainment";
+  categoryObject.imageUrl =
+      "https://images.unsplash.com/photo-1522869635100-9f4c5e86aa37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
+  category.add(categoryObject);
+
+  //3
+  categoryObject = Category();
+  categoryObject.categoryName = "General";
+  categoryObject.imageUrl =
+      "https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+  category.add(categoryObject);
+
+  //4
+  categoryObject = Category();
+  categoryObject.categoryName = "Health";
+  categoryObject.imageUrl =
+      "https://images.unsplash.com/photo-1494390248081-4e521a5940db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1595&q=80";
+  category.add(categoryObject);
+
+  //5
+  categoryObject = Category();
+  categoryObject.categoryName = "Science";
+  categoryObject.imageUrl =
+      "https://images.unsplash.com/photo-1554475901-4538ddfbccc2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1504&q=80";
+  category.add(categoryObject);
+
+  //5
+  categoryObject = Category();
+  categoryObject.categoryName = "Sports";
+  categoryObject.imageUrl =
+      "https://images.unsplash.com/photo-1495563923587-bdc4282494d0?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80";
+  category.add(categoryObject);
+
+  //5
+  categoryObject = Category();
+  categoryObject.categoryName = "Technology";
+  categoryObject.imageUrl =
+      "https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
+  category.add(categoryObject);
+
+  return category;
+}
