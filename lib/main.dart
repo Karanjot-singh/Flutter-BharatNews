612083082ad37dfff9c3072748d1bc805ebfe823
karanjot-singh <karanjot19050@iiitd.ac.in>
2020-08-06 20:45:42 +0530

    minor bug fixes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:provider/provider.dart';
+import 'package:device_preview/device_preview.dart';
 import './blocs/theme.dart';
 import './blocs/theme_options.dart';
 import './views/home.dart';
-import 'package:provider/provider.dart';
 
 void main() {
+  // DevicePreview(builder: (context)=> MyApp);
   runApp(
     // ignore: missing_required_param
     ChangeNotifierProvider<ThemeChanger>(
-      create:(_) => ThemeChanger(lightTheme),
+      create: (_) => ThemeChanger(lightTheme),
       child: MyApp(),
     ),
   );
@@ -17,9 +21,21 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final themeChanger= Provider.of<ThemeChanger>(context);
+    final themeChanger = Provider.of<ThemeChanger>(context);
     return MaterialApp(
-      // title: 'Flutter Demo',
+      builder: (context, widget) => ResponsiveWrapper.builder(
+        BouncingScrollWrapper.builder(context, widget),
+        maxWidth: 1200,
+        minWidth: 450,
+        defaultScale: true,
+        breakpoints: [
+          ResponsiveBreakpoint.resize(450, name: MOBILE),
+          ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+          ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+          ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+        ],
+      ),
       debugShowCheckedModeBanner: false,
       theme: themeChanger.getTheme(),
       home: Home(),
